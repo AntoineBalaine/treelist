504b6c43b42b308393d89a5e55aa36e1c7574c37
Antoine Balaine <abalaineaccts@gmail.com>
2025-06-03 23:26:46 -0400

    typeRegistry + comptimeAllocator

--- src/TypeRegistry.zig
+++ src/TypeRegistry.zig
@@ -8,7 +8,7 @@ pub fn TypeRegistry(comptime TypeStruct: type) type {
             var types: [fields.len]type = undefined;
 
             for (fields, 0..) |field, i| {
-                types[i] = field.defaultValue().?;
+                types[i] = field.type;
             }
 
             break :blk types;
@@ -36,22 +36,39 @@ pub fn TypeRegistry(comptime TypeStruct: type) type {
             });
         };
 
-        // Create the type map at comptime
-        const typeMap = blk: {
-            var kvs: [Types.len]struct { []const u8, u32 } = undefined;
-            // Insert each type with its index
-            for (Types, 0..) |T, i| {
-                kvs[i] = .{ @typeName(T), i };
+        // Context for type lookups
+        const TypeContext = struct {
+            TypesList: [Types.len]type,
+
+            pub fn hash(self: @This(), key: type) u32 {
+                _ = self;
+                return @truncate(std.hash_map.hashString(@typeName(key)));
             }
 
-            break :blk std.StaticStringMap(u32).initComptime(kvs);
+            pub fn eql(ctx: @This(), key: type, b_void: void, b_map_index: usize) bool {
+                _ = b_void;
+                return key == ctx.TypesList[b_map_index];
+            }
+        };
+
+        const typeMap = blk: {
+            var map: std.AutoArrayHashMapUnmanaged(void, void) = .{};
+
+            map.ensureTotalCapacity(comptime_allocator, Types.len) catch unreachable;
+
+            for (Types) |T| {
+                const ctx = TypeContext{ .TypesList = Types };
+                _ = map.getOrPutAssumeCapacityAdapted(T, ctx);
+            }
+
+            break :blk map;
         };
 
         /// Get the TypeId for a given type
-        pub fn idFromType(comptime T: type) TypeId {
-            const name = @typeName(T);
-            const index = typeMap.get(name) orelse
-                @compileError("Type not found in registry: " ++ name);
+        pub fn idFromType(T: type) TypeId {
+            // Use the type context to find the index
+            const ctx = TypeContext{ .TypesList = Types };
+            const index = typeMap.getIndexAdapted(T, ctx).?;
 
             return @as(TypeId, @enumFromInt(index));
         }
@@ -98,95 +115,3 @@ test "TypeRegistry with two fields" {
     try std.testing.expectEqual(id1, Registry.idFromType(u8));
     try std.testing.expectEqual(id2, Registry.idFromType(u16));
 }
-
-// pub const SymbolResolver = struct {
-//     keys: std.ArrayListUnmanaged(Key) = .empty,
-//     values: std.ArrayListUnmanaged(Ref) = .empty,
-//     table: std.AutoArrayHashMapUnmanaged(void, void) = .empty,
-//
-//     const Result = struct {
-//         found_existing: bool,
-//         index: Index,
-//         ref: *Ref,
-//     };
-//
-//     pub fn deinit(resolver: *SymbolResolver, allocator: Allocator) void {
-//         resolver.keys.deinit(allocator);
-//         resolver.values.deinit(allocator);
-//         resolver.table.deinit(allocator);
-//     }
-//
-//     pub fn getOrPut(
-//         resolver: *SymbolResolver,
-//         allocator: Allocator,
-//         ref: Ref,
-//         macho_file: *MachO,
-//     ) !Result {
-//         const adapter = Adapter{ .keys = resolver.keys.items, .macho_file = macho_file };
-//         const key = Key{ .index = ref.index, .file = ref.file };
-//         const gop = try resolver.table.getOrPutAdapted(allocator, key, adapter);
-//         if (!gop.found_existing) {
-//             try resolver.keys.append(allocator, key);
-//             _ = try resolver.values.addOne(allocator);
-//         }
-//         return .{
-//             .found_existing = gop.found_existing,
-//             .index = @intCast(gop.index + 1),
-//             .ref = &resolver.values.items[gop.index],
-//         };
-//     }
-//
-//     pub fn get(resolver: SymbolResolver, index: Index) ?Ref {
-//         if (index == 0) return null;
-//         return resolver.values.items[index - 1];
-//     }
-//
-//     pub fn reset(resolver: *SymbolResolver) void {
-//         resolver.keys.clearRetainingCapacity();
-//         resolver.values.clearRetainingCapacity();
-//         resolver.table.clearRetainingCapacity();
-//     }
-//
-//     const Key = struct {
-//         index: Symbol.Index,
-//         file: File.Index,
-//
-//         fn getName(key: Key, macho_file: *MachO) [:0]const u8 {
-//             const ref = Ref{ .index = key.index, .file = key.file };
-//             return ref.getSymbol(macho_file).?.getName(macho_file);
-//         }
-//
-//         pub fn getFile(key: Key, macho_file: *MachO) ?File {
-//             const ref = Ref{ .index = key.index, .file = key.file };
-//             return ref.getFile(macho_file);
-//         }
-//
-//         fn eql(key: Key, other: Key, macho_file: *MachO) bool {
-//             const key_name = key.getName(macho_file);
-//             const other_name = other.getName(macho_file);
-//             return mem.eql(u8, key_name, other_name);
-//         }
-//
-//         fn hash(key: Key, macho_file: *MachO) u32 {
-//             const name = key.getName(macho_file);
-//             return @truncate(Hash.hash(0, name));
-//         }
-//     };
-//
-//     const Adapter = struct {
-//         keys: []const Key,
-//         macho_file: *MachO,
-//
-//         pub fn eql(ctx: @This(), key: Key, b_void: void, b_map_index: usize) bool {
-//             _ = b_void;
-//             const other = ctx.keys[b_map_index];
-//             return key.eql(other, ctx.macho_file);
-//         }
-//
-//         pub fn hash(ctx: @This(), key: Key) u32 {
-//             return key.hash(ctx.macho_file);
-//         }
-//     };
-//
-//     pub const Index = u32;
-// };
